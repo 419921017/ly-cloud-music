8a44e46e41691d9dda1d0fabb7d35407aaa5a792
power_840 <liuyu19921017@gmail.com>
2020-02-11 16:51:59 +0800

    feat: redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { HashRouter } from 'react-router-dom';
 import { renderRoutes } from 'react-router-config';
+import { Provider } from 'react-redux';
+
+import store from './store';
 import routes from './routes';
 import { GlobalStyle } from './style';
 import { IconStyle } from './assets/iconfont/iconfont';
 
 function App() {
   return (
-    <HashRouter>
-      <GlobalStyle />
-      <IconStyle />
-      {renderRoutes(routes)}
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle />
+        <IconStyle />
+        {renderRoutes(routes)}
+      </HashRouter>
+    </Provider>
   );
 }
 
