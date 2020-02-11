c4590ba1f16a288d92c81987602f2765e4eb232f
power_840 <liuyu19921017@gmail.com>
2020-02-11 16:43:34 +0800

    feat: home Top Tab TabItem

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { HashRouter } from 'react-router-dom';
+import { renderRoutes } from 'react-router-config';
+import routes from './routes';
 import { GlobalStyle } from './style';
-import {IconStyle} from './assets/iconfont/iconfont';
+import { IconStyle } from './assets/iconfont/iconfont';
 
 function App() {
   return (
-    <div className="App">
-      <GlobalStyle/>
-      <IconStyle/>
-      <i className="iconfont">&#xe62b;</i>
-    </div>
+    <HashRouter>
+      <GlobalStyle />
+      <IconStyle />
+      {renderRoutes(routes)}
+    </HashRouter>
   );
 }
 
