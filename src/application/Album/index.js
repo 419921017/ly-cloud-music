bf9b3a9a8f4975235fa7c8c1aa1756c5ae1458fd
power_840 <liuyu19921017@gmail.com>
2020-02-13 18:31:03 +0800

    feat: transition

--- src/application/Album/index.js
+++ src/application/Album/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { CSSTransition } from 'react-transition-group';
+
 import { Container } from './style';
+import Header from './../../baseUI/header';
 
 function Album(props) {
-  const {route} = props
-  console.log('Album', route)
-  return <Container>123</Container>;
+  const [showStatus, setShowStatus] = useState (true);
+
+  const handleBack = () => {
+    setShowStatus (false);
+  };
+
+  return (
+    <CSSTransition
+      in={showStatus}
+      timeout={300}
+      classNames="fly"
+      appear={true}
+      unmountOnExit
+      onExited={props.history.goBack}
+    >
+      <Container>
+        <Header title={"返回"} handleClick={handleBack}></Header>
+      </Container>
+    </CSSTransition>
+  );
 }
 
 export default React.memo(Album);
